ubports-clover/android_kernel_xiaomi_clover

drivers/gpio: Import Xiaomi changes

drivers/gpio/gpiolib.c
@@ -1123,12 +1123,14 @@ static int _gpiod_direction_output_raw(struct gpio_desc *desc, int value)
 	int			status = -EINVAL;
 
 	/* GPIOs used for IRQs shall not be set as output */
+#ifndef CONFIG_XIAOMI_CLOVER
 	if (test_bit(FLAG_USED_AS_IRQ, &desc->flags)) {
 		gpiod_err(desc,
 			  "%s: tried to set a GPIO tied to an IRQ as output\n",
 			  __func__);
 		return -EIO;
 	}
+#endif
 
 	/* Open drain pin should not be driven to 1 */
 	if (value && test_bit(FLAG_OPEN_DRAIN,  &desc->flags))

yourbestregard/android_kernel_xiaomi_southwest-4.19

(cherry picked from commit 9c3f079c4fcf1262b035372fc830981fd82b9d6c)
Signed-off-by: pix106 <sbordenave@gmail.com>

drivers/gpio/gpiolib.c
@@ -2673,12 +2673,14 @@ int gpiod_direction_output(struct gpio_desc *desc, int value)
 		value = !!value;
 
 	/* GPIOs used for IRQs shall not be set as output */
+#ifndef CONFIG_MACH_XIAOMI_CLOVER
 	if (test_bit(FLAG_USED_AS_IRQ, &desc->flags)) {
 		gpiod_err(desc,
 			  "%s: tried to set a GPIO tied to an IRQ as output\n",
 			  __func__);
 		return -EIO;
 	}
+#endif
 
 	gc = desc->gdev->chip;
 	if (test_bit(FLAG_OPEN_DRAIN, &desc->flags)) {